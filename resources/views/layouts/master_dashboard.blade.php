f4d7ee331c0bae07835dc05f9e74a07d4d6f90ed
Irfa Ardiansyah <irfardy2@gmail.com>
2022-07-16 21:04:22 +0700

    Remove CP

--- resources/views/layouts/master_dashboard.blade.php
+++ resources/views/layouts/master_dashboard.blade.php
@@ -268,9 +268,9 @@
 
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2022 Indihealth.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022 Sanggar Tari Ayunindya.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
